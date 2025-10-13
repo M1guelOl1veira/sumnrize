2d4a1b1fa14bf5875f556ea4bdb3d2741427a218
Miguel Oliveira <miguel.ao1086@gmail.com>
2025-10-13 17:11:57 -0300

    Configurando prettier

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,13 @@
 function Home() {
-    return (
-        <div style={{ textAlign: "center", marginTop: "40px" }}>
-            <h1>Nicole, te amo!</h1>
-            <img
-                src="/images/image.png"
-                alt="Gatinho fofo segurando panetone"
-                style={{ width: "300px", borderRadius: "16px", marginTop: "20px" }}
-            />
-        </div>
-    );
+  return (
+    <div style={{ textAlign: "center", marginTop: "40px" }}>
+      <h1>Nicole, te amo!</h1>
+      <img
+        src="/images/image.png"
+        alt="Gatinho fofo segurando panetone"
+        style={{ width: "300px", borderRadius: "16px", marginTop: "20px" }}
+      />
+    </div>
+  );
 }
-
 export default Home;
