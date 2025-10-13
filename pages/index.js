a6e2f7cf889bd4fd7f8cf78a74bb6f242abd8bf8
Miguel Oliveira <miguel.ao1086@gmail.com>
2025-10-13 14:22:41 -0300

    Mudando msg e adicionando imagem

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,14 @@
 function Home() {
-    return <h1>Nicole, te amo! Se você também me ama da uma risadinha!</h1>
+    return (
+        <div style={{ textAlign: "center", marginTop: "40px" }}>
+            <h1>Nicole, te amo! (manda uma fotinha)</h1>
+            <img
+                src="/images/image.png"
+                alt="Gatinho fofo segurando panetone"
+                style={{ width: "300px", borderRadius: "16px", marginTop: "20px" }}
+            />
+        </div>
+    );
 }
 
 export default Home;
